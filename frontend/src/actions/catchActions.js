4669ede2e7fb7284256d5b40b6ea54424edafd34
keserukristof <41549423+keserukristof@users.noreply.github.com>
2020-05-10 17:34:16 +0200

    redux fetch

--- frontend/src/actions/catchActions.js
+++ frontend/src/actions/catchActions.js
@@ -1,16 +1,13 @@
 import { FETCH_CATCHES, NEW_CATCH } from './types';
 import axios from 'axios';
 
-export const fetchCatches = () => dispatch => {
+export const fetchCatches = () => (dispatch) => {
   //HA VALAMI ELBASZÓDIK EZT NÉZD MEG ELŐSZÖR!!!!
-    axios.get('http://localhost:3001/catches?id=101')
-      .then((catches) =>
-        dispatch({
-          type: FETCH_CATCHES,
-          payload: catches
-        })
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+  fetch('http://localhost:3001/catches?id=101')
+  .then((res) => res.json())
+  .then(catches =>
+    dispatch({
+      type: FETCH_CATCHES,
+      payload: catches
+    }))
+};
